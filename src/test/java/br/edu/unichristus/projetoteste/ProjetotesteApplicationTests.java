d054ed50ebadf3107fd4b5d347e1e8d787d35ed9
Rosa Vasconcelos <rcandidacursos@yahoo.com>
2023-11-02 17:38:22 -0300

    Initial commit

---
+++ src/test/java/br/edu/unichristus/projetoteste/ProjetotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unichristus.projetoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
